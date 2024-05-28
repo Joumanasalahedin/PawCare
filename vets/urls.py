e2d9d28a8e49a1c08c1f78a2326450c3104f9141
Joumana <joumanasalahedin@gmail.com>
2024-05-29 00:00:46 +0200

    doc and owners login and register

--- vets/urls.py
+++ vets/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import doctor_dashboard
+from .views import doctor_dashboard, doc_register_login, password_reset
 
 urlpatterns = [
     path('dashboard', doctor_dashboard, name='doctor_dashboard'),
-    # Add other URL patterns here
+    path('signin-register', doc_register_login, name='doc_register_login'),
+    path('password_reset', password_reset, name='password_reset'),
 ]
--- vets/views.py
+++ vets/views.py
@@ -2,5 +2,13 @@ from django.shortcuts import render, get_object_or_404
 from .models import Vet
 
 
+def doc_register_login(request):
+    return render(request, 'drl.html')
+
+
+def password_reset(request):
+    return render(request, 'pass_reset.html')
+
+
 def doctor_dashboard(request):
     return render(request, 'doctor_dashboard.html')
